b3ae6b38c457ef22d8c780fc213b9a6dae111043
Noah <celestiarune@icloud.com>
2024-06-28 17:04:12 +0900

    copy project

---
+++ ccal/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ExcelRowSplitter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnAttachFile = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnAttachFile
+            // 
+            this.btnAttachFile.Location = new System.Drawing.Point(12, 12);
+            this.btnAttachFile.Name = "btnAttachFile";
+            this.btnAttachFile.Size = new System.Drawing.Size(125, 23);
+            this.btnAttachFile.TabIndex = 0;
+            this.btnAttachFile.Text = "데이터파일 첨부";
+            this.btnAttachFile.UseVisualStyleBackColor = true;
+            this.btnAttachFile.Click += new System.EventHandler(this.btnAttachFile_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(143, 17);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 15);
+            this.lblFilePath.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 50);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(760, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnAttachFile);
+            this.Name = "Form1";
+            this.Text = "정산서 발급기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btnAttachFile;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
